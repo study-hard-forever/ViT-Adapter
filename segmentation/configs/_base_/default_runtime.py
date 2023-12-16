56a28e96d43a37b9745603ed5fc73758a1efa3fc
study_forever <1551183645@qq.com>
2023-12-16 11:25:40 +0000

    test

--- segmentation/configs/_base_/default_runtime.py
+++ segmentation/configs/_base_/default_runtime.py
@@ -12,3 +12,19 @@ load_from = None
 resume_from = None
 workflow = [('train', 1)]
 cudnn_benchmark = True
+
+'''
+可视化（未添加）：
+监控训练时候的状态：
+启动时添加地址：--work-dir work_dir/test_visual
+示例如下：python tools/train.py configs/pspnet/pspnet_r50-d8_4xb4-80k_ade20k-512x512.py --work-dir work_dir/test_visual
+参考链接：https://mmsegmentation.readthedocs.io/zh-cn/main/user_guides/visualization.html#id2
+注：此处来自文档mmsegmentation1.2.2,此处版本为0.20.2，略有不同，如0.20.2版本的checkpoint配置为checkpoint_config，而1.2.2文档中checkpoint配置包含在default_hooks中，为checkpoint
+'''
+
+'''
+vis_backends = [dict(type='LocalVisBackend'),
+                dict(type='TensorboardVisBackend')]
+visualizer = dict(
+    type='SegLocalVisualizer', vis_backends=vis_backends, name='visualizer')
+'''
--- segmentation/configs/_base_/schedules/schedule_80k.py
+++ segmentation/configs/_base_/schedules/schedule_80k.py
@@ -12,3 +12,20 @@ evaluation = dict(interval=8000, metric='mIoU', pre_eval=True)
 # runner = dict(type='EpochBasedRunner', max_epochs=100)  # 按epoch的方式进行迭代
 # checkpoint_config = dict(by_epoch=True, interval=1)  # 每多少次迭代保存一次模型
 # evaluation = dict(interval=1, metric='mIoU')  # 每多少次迭代计算一次指标
+
+'''
+可视化（未添加）：
+数据和结果的可视化：模型测试或验证期间的可视化数据样本
+MMSegmentation 提供了 SegVisualizationHook ，它是一个可以用于可视化 ground truth 和在模型测试和验证期间的预测分割结果的钩子 。 它的配置在 default_hooks 中
+参考链接：https://mmsegmentation.readthedocs.io/zh-cn/main/user_guides/visualization.html#id2
+注：此处来自文档mmsegmentation1.2.2,此处版本为0.20.2，略有不同，如上述checkpoint配置为checkpoint_config而下面的都包含在default_hooks中，为checkpoint
+'''
+'''
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=2000),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook', draw=True, interval=1))
+'''
